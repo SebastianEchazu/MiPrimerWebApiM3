22f564a36cb922bf0a09b4640cc13b1b6e74439c
Ejemplo de pagina personal <sebastianechazu@outlook.com>
2021-05-05 14:31:55 -0300

    cambios en el modelo autor

--- MiPrimerWebApiM3/Entities/Autor.cs
+++ MiPrimerWebApiM3/Entities/Autor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,15 @@ namespace MiPrimerWebApiM3.Entities
     public class Autor
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "El campo debe tener al menos {1} caracter")]
         public string Nombre { get; set; }
+        [Range(18, 120)]// la edad debe ser entre 18 y 120
+        public int edad { get; set; }
+        [CreditCard]
+        public string CreditCard { get; set; }
+        [Url]
+        public string Url { get; set; }
         public List<Libro> Libros { get; set; }
     }
 }
